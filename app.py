5c256784068b9a61132249ed1d269e8ac34010bc
jsebastian2707 <juansebherndez@gmail.com>
2024-09-17 21:18:24 -0500

    added auto search for birds and resize funtions

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
 from PIL import Image
 from rembg import remove
+import cv2
+
+def getfolders(folder_path):
+    return os.listdir(folder_path)
 
 def crop_images_in_folder(folder_path, output_size=(224, 224)):
     """
     Itera a través de las imágenes en una carpeta y las recorta al tamaño especificado.
-    Guarda las imágenes recortadas en un subdirectorio llamado 'cropped_images'.
+    Guarda las imágenes recortadas en un subdirectorio llamado 'result'.
     
     Args:
     folder_path (str): Ruta a la carpeta que contiene las imágenes
     output_size (tuple): Tamaño de salida deseado (ancho, alto)
     """
-    output_folder = os.path.join(folder_path, "cropped_images")
+    output_folder = os.path.join(folder_path, "result")
     os.makedirs(output_folder, exist_ok=True)
 
     for filename in os.listdir(folder_path):
@@ -31,6 +35,64 @@ def crop_images_in_folder(folder_path, output_size=(224, 224)):
                 cropped_img.save(output_path)
 
     print(f"Todas las imagenes han sido cortadas. Estan guardadas en: {output_folder}")
+
+
+def resize_images_in_folder(folder_path, output_size=(224, 224)):
+    # Crear carpetas de salida
+    output_folder = os.path.join(folder_path, "cropped_images_square")
+    output_folder2 = os.path.join(folder_path, "result")
+    os.makedirs(output_folder, exist_ok=True)
+    os.makedirs(output_folder2, exist_ok=True)
+
+    # Parte 1: Recortar las imágenes para hacerlas cuadradas
+    for filename in os.listdir(folder_path):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+            file_path = os.path.join(folder_path, filename)
+            with Image.open(file_path) as img:
+                width, height = img.size
+                
+                # Definir el área de recorte para hacer la imagen cuadrada
+                if width > height:
+                    new_width = height
+                    left = (width - new_width) // 2
+                    upper = 0
+                    right = left + new_width
+                    lower = height
+                else:
+                    new_height = width
+                    left = 0
+                    upper = (height - new_height) // 2
+                    right = width
+                    lower = upper + new_height
+
+                # Recortar la imagen
+                cropped_img = img.crop((left, upper, right, lower))
+                
+                # Guardar la imagen recortada
+                output_filename = f"cropped_{filename}"
+                output_path = os.path.join(output_folder, output_filename)
+                cropped_img.save(output_path)
+
+    # Parte 2: Redimensionar las imágenes recortadas
+    for filename in os.listdir(output_folder):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+            file_path = os.path.join(output_folder, filename)
+            
+            # Leer la imagen recortada con OpenCV
+            image = cv2.imread(file_path)
+            
+            if image is not None:  # Verificar si la imagen se cargó correctamente
+                # Redimensionar la imagen al tamaño deseado
+                resized_image = cv2.resize(image, output_size)
+                
+                # Generar nombre de archivo de salida
+                output_filename = f"resize_{filename}"
+                output_path = os.path.join(output_folder2, output_filename)
+                
+                # Guardar la imagen redimensionada
+                cv2.imwrite(output_path, resized_image)
+
+    print(f"Todas las imágenes han sido recortadas y redimensionadas. Las imágenes recortadas están en: {output_folder}, y las redimensionadas en: {output_folder2}")
 
 def remove_background(input_folder, output_folder):
     
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,55 @@
 import streamlit as st
 import os
-from app import crop_images_in_folder, remove_background
+from app import getfolders, crop_images_in_folder,resize_images_in_folder, remove_background
+import pandas as pd
+
 
 def main():
     st.title("Procesamiento de Imágenes")
-
-    base_folder = st.text_input("Ruta de la carpeta de imágenes:", "C:/Users/dmedina/Documents/Daniel/Ucc/10mo/Electiva 3/Prueba/Accipiter sp")
+    base_folder = st.text_input("Ruta del dataset", "E:\OneDrive - Universidad Cooperativa de Colombia\entrega dataset")
+    especies=getfolders(base_folder)
+    data =[]
+    for  especie in especies:
+        data.append({ "name": especie, "procesar": False})
+    df = pd.DataFrame(data)
+    edited_especies = st.data_editor(df,width =500,column_config={"name": st.column_config.TextColumn(label="🐦")},disabled=["name"])
+    favorite_command = edited_especies.loc[edited_especies['procesar']==True]["name"]
     
-    col1, col2 = st.columns(2)
+    folders = []
+    for row in favorite_command.values:
+        folders.append(row+"")
 
+    percent_complete=0
+    step=1/len(folders) if len(folders)>0 else 0.1
+
+    progress_text = "procesando, por favor espere ..."
+    my_bar = st.progress(0, text=progress_text)
+    col1, col2 ,col3 = st.columns(3)
     with col1:
-        if st.button("Recortar Imágenes"):
-            crop_images_in_folder(base_folder)
-            st.success("Imágenes recortadas con éxito!")
-
-    with col2:
+        on = st.toggle("")
+    if on:
+        with col2:
+            if st.button("Redimensionar Imágenes"):
+                for folder in folders:
+                    resize_images_in_folder(f"{base_folder}\{folder}")
+                    my_bar.progress(percent_complete + step, text=progress_text)
+                my_bar.empty()
+                st.success("Imágenes recortadas y redimensionadas con éxito!")
+    else:
+        with col2:
+            if st.button("Recortar Imágenes"):
+                for folder in folders:
+                    crop_images_in_folder(f"{base_folder}\{folder}")
+                    my_bar.progress(percent_complete + step, text=progress_text)
+                my_bar.empty()
+                st.success("Imágenes recortadas con éxito!")
+    with col3:
         if st.button("Remover Fondo"):
-            no_bg_folder = os.path.join(base_folder, "no_background")
-            remove_background(base_folder, no_bg_folder)
+            for folder in folders:
+                no_bg_folder = os.path.join(f"{base_folder}\{folder}", "no_background")
+                remove_background(f"{base_folder}\{folder}\\result", no_bg_folder)
+                my_bar.progress(percent_complete + step, text=progress_text)
+            my_bar.empty()
             st.success("Fondo removido de las imágenes con éxito!")
 
 if __name__ == "__main__":
